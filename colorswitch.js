5cc52facdfd570579e5e5eba84eb371661116050
Isaac Molina <isaacmolina.m16@gmail.com>
2020-06-30 19:18:27 -0500

    added 4 colors

--- colorswitch.js
+++ colorswitch.js
@@ -2,6 +2,10 @@ document.getElementById('grayButton').onclick = switchGray;
 document.getElementById('whiteButton').onclick = switchWhite;
 document.getElementById('blueButton').onclick = switchBlue;
 document.getElementById('yellowButton').onclick = switchYellow;
+document.getElementById('redButton').onclick = switchRed;
+document.getElementById('purpleButton').onclick = switchPurple;
+document.getElementById('orangeButton').onclick = switchOrange;
+document.getElementById('greenButton').onclick = switchGreen;
 
 function switchGray() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "gray";
@@ -18,4 +22,20 @@ function switchBlue() {
 function switchYellow() {
     document.getElementsByTagName("body")[0].style.backgroundColor = "yellow";
     document.getElementsByTagName("body")[0].style.color = "black";
+}
+function switchRed() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+    document.getElementsByTagName("body")[0].style.color = "white";
+}
+function switchPurple(){
+    document.getElementsByTagName("body")[0].style.backgroundColor = "purple";
+    document.getElementsByTagName("body")[0].style.color = "black";
+}
+function switchOrange(){
+    document.getElementsByTagName("body")[0].style.backgroundColor = "orange";
+    document.getElementsByTagName("body")[0].style.color = "black";
+}
+function switchGreen(){
+    document.getElementsByTagName("body")[0].style.backgroundColor = "green";
+    document.getElementsByTagName("body")[0].style.color = "white";
 }
